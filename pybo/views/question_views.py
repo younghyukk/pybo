6bc3dfa6ef2383427f75845db9d998b71868ee11
Younghyuk Kim <younghyukk@gmail.com>
2024-09-02 20:48:44 -0400

    flask study

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -17,7 +17,7 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
-    form = AnswerForm
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
     return render_template(
         'question/question_detail.html', question=question, form=form
